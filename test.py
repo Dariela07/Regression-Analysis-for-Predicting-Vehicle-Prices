84e44221f0989fa8b888ab672bcfd92b2b8dbdb6
yixuan <yixuan@yixuans-MacBook-Pro.local>
2024-05-26 19:11:52 +0100

    Deleted test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# create DataFrame
-df = pd.DataFrame({'store': ['A', 'A', 'A', 'A', 'B', 'B', 'B', 'B'],
-                   'sales': [12, 15, 24, 24, 14, 19, 12, 38],
-                   'refunds': [4, 8, 7, 7, 10, 5, 4, 11]})
-
-print(df.groupby('store').get_group('A')['sales'])
